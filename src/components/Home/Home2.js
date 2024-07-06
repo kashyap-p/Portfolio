5af00f3899915e2d49d76637aaabab194cb25228
Kashyap Patel <43714430+kashyap-p@users.noreply.github.com>
2024-07-06 13:01:54 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -63,7 +63,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/Stark0702"
+                  href="https://github.com/kashyap-p"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
